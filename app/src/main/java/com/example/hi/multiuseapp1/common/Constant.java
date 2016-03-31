75573e2765aa3d5de026df0db7136c9f3428b888
diks <diks>
2016-03-31 13:49:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hi/multiuseapp1/common/Constant.java
@@ -0,0 +1,18 @@
+package com.example.hi.multiuseapp1.common;
+
+/**
+ * Created by hi on 3/31/2016.
+ */
+public class Constant {
+    public static final int NOTEPAD=1;
+    public static final int TODO=2;
+    public static final int DRAWING=3;
+    public static final int REMINDER=4;
+    public static final int MOVIE=5;
+    public static final int SETTING=6;
+    public static final String PREFERENCE_NAME="app_preference";
+    public static final String DEFAULT_APP="default_app";
+
+
+
+}
